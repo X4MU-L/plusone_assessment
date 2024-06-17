c3785bd04b541236cfc8d2143281528a20f1ccab
chukwuebuka <okolisamuel21@gmail.com>
2024-06-17 09:25:14 +0100

    connect to mongoDB using mongoose

--- server/mongo.ts
+++ server/mongo.ts
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 export const mongoConnect = async (): Promise<void> => {
   try {
     mongoose.set("strictQuery", false);
-    const conn = await mongoose.connect(process.env.MONGODB_URL!, {});
+    const conn = await mongoose.connect(process.env.MONGODB_URL!);
     console.log(`Connected to MongoDB ${conn.connection.host}`);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
