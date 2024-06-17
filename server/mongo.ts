013bf89a224153dd3a7df5863837588fb4ceab70
chukwuebuka <okolisamuel21@gmail.com>
2024-06-17 09:24:42 +0100

    connect to mongodd using mongoose

---
+++ server/mongo.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+export const mongoConnect = async (): Promise<void> => {
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(process.env.MONGODB_URL!, {});
+    console.log(`Connected to MongoDB ${conn.connection.host}`);
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+export default mongoConnect;
