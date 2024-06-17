9e8754c40ed0403e22a45fabef075b629db43997
chukwuebuka <okolisamuel21@gmail.com>
2024-06-17 12:06:49 +0100

    generate types from joi schema for development environment with typescript

---
+++ server/scripts/start-dev.ts
@@ -0,0 +1,31 @@
+import { convertFromDirectory } from "joi-to-typescript";
+import { spawn } from "child_process";
+
+async function generateTypesAndStartServer() {
+  // Generate types from Joi schemas
+  const result = await convertFromDirectory({
+    schemaDirectory: "schema",
+    typeOutputDirectory: "types",
+    debug: true,
+  });
+
+  if (result) {
+    console.log("Types generated successfully");
+  } else {
+    console.log("Failed to generate types");
+  }
+
+  // Start your server
+  const serverProcess = spawn("ts-node", ["server.ts"], {
+    stdio: "inherit",
+    shell: true,
+  });
+
+  // Handle process exit
+  serverProcess.on("exit", (code) => {
+    console.log(`Server process exited with code ${code}`);
+    process.exit(code);
+  });
+}
+
+generateTypesAndStartServer();
--- server/server.ts
+++ server/server.ts
@@ -2,12 +2,32 @@ import dotenv from "dotenv";
 import express from "express";
 import blogPostRoute from "./routes/v1/blog-post";
 import mongoConnect from "./mongo";
+import cors, { CorsOptions } from "cors";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5009;
 
 mongoConnect();
+
+const allowedOrigins = ["http://localhost:3001"];
+
+const corsOptions: CorsOptions = {
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "HEAD,POST",
+  optionsSuccessStatus: 200,
+};
+
+app.use(express.json());
+app.use(cors(corsOptions));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.use("/api/v1/blog-post", blogPostRoute);
 
