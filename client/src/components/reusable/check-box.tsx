4888719b16411d4169d7f19730add8593b59ccb0
chukwuebuka <okolisamuel21@gmail.com>
2024-06-18 02:18:14 +0100

    create check box

---
+++ client/src/components/reusable/check-box.tsx
@@ -0,0 +1,21 @@
+function CheckBox({ label }: { label: string }) {
+  return (
+    <div className="flex items-start">
+      <div className="flex items-center h-5">
+        <input
+          id="remember"
+          aria-describedby="remember"
+          type="checkbox"
+          className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+        />
+      </div>
+      <div className="ml-3 text-sm">
+        <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">
+          {label}
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default CheckBox;
