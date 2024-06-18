42a5a08d8efadd4010e778b4d15fd983d4c3da01
chukwuebuka <okolisamuel21@gmail.com>
2024-06-18 02:05:52 +0100

    create Input component

---
+++ client/src/components/input.tsx
@@ -0,0 +1,29 @@
+type InputPropsType = {
+  type: string;
+  name: string;
+  id: string;
+  placeholder: string;
+  label: string;
+};
+
+function Input({ type, name, id, placeholder, label }: InputPropsType) {
+  return (
+    <div>
+      <label
+        htmlFor={id}
+        className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+      >
+        {label}
+      </label>
+      <input
+        type={type}
+        name={name}
+        id={id}
+        placeholder={placeholder}
+        className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+    </div>
+  );
+}
+
+export default Input;
